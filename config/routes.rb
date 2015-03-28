fe4b27656d6860cb8bbb726ceca0003c2a00c3a3
Takuya Mukohira <takuya.mk96@gmail.com>
2015-03-28 19:59:48 +0900

    Event register

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :events
+
   root to: 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: :logout
