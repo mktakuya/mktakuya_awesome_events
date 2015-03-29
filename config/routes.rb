034f83bfe6d4d0b269af697a36532cd8600b381a
Takuya Mukohira <takuya.mk96@gmail.com>
2015-03-29 19:07:59 +0900

    To clear routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :events do
-    resources :tickets
-  end
+  root to: 'welcome#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: :logout
 
   resource :user do
     get 'retire'
   end
 
-  root to: 'welcome#index'
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy', as: :logout
+  resources :events do
+    resources :tickets
+  end
 end
