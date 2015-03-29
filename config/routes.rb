2fdfd5896628311d1027bfd5408ac709d5455a4d
Takuya Mukohira <takuya.mk96@gmail.com>
2015-03-29 19:00:53 +0900

    Delete user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :tickets
   end
 
+  resource :user do
+    get 'retire'
+  end
+
   root to: 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: :logout
