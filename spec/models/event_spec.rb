cf5852776e7680d0c710b36ebe19d34a2b4798d4
Takuya Mukohira <takuya.mk96@gmail.com>
2015-03-30 02:17:35 +0900

    tdd

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -20,4 +20,20 @@ describe Event do
       it { should be_truthy }
     end
   end
+
+  describe '#rails?' do
+    context '#nameが "Rails勉強会" のとき' do
+      it 'trueを返すこと' do
+        event = create(:event, name: 'Rails勉強会')
+        expect(event.rails?).to eq true
+      end
+    end
+
+    context '#nameが "Ruby勉強会" のとき' do
+      it 'falseを返すこと' do
+        event = create(:event, name: 'Ruby勉強会')
+        expect(event.rails?).to eq false
+      end
+    end
+  end
 end
